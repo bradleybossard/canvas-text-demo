b248a1df3162d69255846580efaf7220fcd3ef8a
Bradley Bossard <bradleybossard@gmail.com>
2016-09-26 20:08:45 +0000

    Added Google Font Loader

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function draw() {
-  var ctx = document.getElementById('canvas').getContext('2d');
-  ctx.font = "48px serif";
-  ctx.fillText("Hello world", 10, 50);
-}
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-draw();
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+
+	WebFontConfig = {
+		google: {
+			//families: ['Droid Sans', 'Droid Serif']
+			//families: ['Droid Sans']
+			families: ['Yatra One']
+		},
+
+		loading: function() {},
+		active: function() {},
+		inactive: function() {},
+		fontloading: function(familyName, fvd) {},
+		fontactive: function(familyName, fvd) {
+			//ctx.font = '50px "Droid Sans"';
+			ctx.font = '50px "Yatra One"';
+			ctx.textBaseline = 'top';
+			ctx.fillText('Hello!', 20, 10);
+		},
+		fontinactive: function(familyName, fvd) {}
+	};
+
+	(function(d) {
+		var wf = d.createElement('script'), s = d.scripts[0];
+		wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+		s.parentNode.insertBefore(wf, s);
+	})(document);
+
+});
