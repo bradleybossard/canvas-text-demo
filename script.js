e4d4e80a0f0ad19f94acf4f7d52e6dbb8587e0b6
Bradley Bossard <bradleybossard@gmail.com>
2016-09-27 22:09:02 +0000

    Rename text pos variables

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var app = angular.module('myApp', [])
 .controller('myCtrl', function($scope, $http) {
 
-  $scope.xPos = 0;
-  $scope.yPos = 0;
+  $scope.xTextPos = 0;
+  $scope.yTextPos = 0;
   $scope.strokeColor = '#ff0000';
   $scope.fillColor = '#00ff00';
   $scope.textContent = 'Hello World';
@@ -40,9 +40,9 @@ var app = angular.module('myApp', [])
 		textCtx.font = '50px "Yatra One"';
 		textCtx.textBaseline = 'top';
 		textCtx.fillStyle = $scope.fillColor; // line color
-		textCtx.fillText($scope.textContent, $scope.xPos, $scope.yPos);
+		textCtx.fillText($scope.textContent, $scope.xTextPos, $scope.yTextPos);
 		textCtx.strokeStyle = $scope.strokeColor; // line color
-		textCtx.strokeText($scope.textContent, $scope.xPos, $scope.yPos);
+		textCtx.strokeText($scope.textContent, $scope.xTextPos, $scope.yTextPos);
   }
 
   function drawImage() {
